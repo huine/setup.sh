01a025b30d9d7ac7505476196ba90dcecdc1c0cc
gabrielgisoldo <gabrielgisoldi@hotmail.com>
2017-05-23 15:16:38 -0300

    Adicionada verificacao de pacotes.

--- setup.sh
+++ setup.sh
@@ -1,162 +1,172 @@
 ######################## Começa inicialização de telas ########################
 
-exec 2> /dev/null
+wmctrl_pkg=$(ls /var/log/packages | grep wmctrl)
+sbotools=$(ls /var/log/packages | grep sbotools)
 
-####################### Inicia WorkSpace 0 ####################################
+if [ ${#wmctrl_pkg} -gt 0 ]; then
 
-wmctrl -s 0
+    exec 2> /dev/null
 
-############# Inicia procs com Eyebeam #############
+    ####################### Inicia WorkSpace 0 ####################################
 
-wine "C:\Program Files (x86)\CounterPath\eyeBeam 1.5\eyeBeam.exe" &>/dev/null &
-while [ "$WID" == "" ]; do
-    WID=$(wmctrl -lp | sort -k2 | grep -i "eyeBeam" | awk '{print $1, $2}' | grep -P "^\d{1}x(\d|[a-fA-F])+\s0" | awk '{print $1}' )
-done
-wmctrl -i -r $WID -b add,hidden,below
+    wmctrl -s 0
 
-############## Fim Eyebeam ##############
+    ############# Inicia procs com Eyebeam #############
 
-############# Inicia procs com Firefox #############
+    wine "C:\Program Files (x86)\CounterPath\eyeBeam 1.5\eyeBeam.exe" &>/dev/null &
+    while [ "$WID" == "" ]; do
+        WID=$(wmctrl -lp | sort -k2 | grep -i "eyeBeam" | awk '{print $1, $2}' | grep -P "^\d{1}x(\d|[a-fA-F])+\s0" | awk '{print $1}' )
+    done
+    wmctrl -i -r $WID -b add,hidden,below
 
-WID=""
-firefox "totalip5.nube" "nubenet.nube\nube" &>/dev/null &
-while [ "$WID" == "" ]; do
-    WID=$(wmctrl -lp | sort -k2 | grep -i "Mozilla Firefox" | awk '{print $1, $2}' | grep -P "^\d{1}x(\d|[a-fA-F])+\s0" | awk '{print $1}' )
-done
-wmctrl -i -r $WID -e 0,1367,102,1920,1016
+    ############## Fim Eyebeam ##############
 
-############## Fim Firefox ##############
+    ############# Inicia procs com Firefox #############
 
-############# Inicia procs com terminator #############
+    WID=""
+    firefox "totalip5.nube" "nubenet.nube\nube" &>/dev/null &
+    while [ "$WID" == "" ]; do
+        WID=$(wmctrl -lp | sort -k2 | grep -i "Mozilla Firefox" | awk '{print $1, $2}' | grep -P "^\d{1}x(\d|[a-fA-F])+\s0" | awk '{print $1}' )
+    done
+    wmctrl -i -r $WID -e 0,1367,102,1920,1016
 
-terminator --geometry=683x731+1+120 #Segundo monitor - terminator da esquerda
-terminator --geometry=683x731+693+120 -l terminator_direita #Segundo monitor - terminator da direita
+    ############## Fim Firefox ##############
 
-############## Fim terminator ##############
+    ############# Inicia procs com terminator #############
 
-######################## Fim WorkSpace 0 ######################################
+    terminator --geometry=683x731+1+120 #Segundo monitor - terminator da esquerda
+    terminator --geometry=683x731+693+120 -l terminator_direita #Segundo monitor - terminator da direita
 
+    ############## Fim terminator ##############
 
-####################### Inicia WorkSpace 1 ####################################
-
-wmctrl -s 1
-
-############# Inicia procs com Firefox #############
-
-WID=""
-firefox --new-window "localhost/nube" &>/dev/null &
-while [ "$WID" == "" ]; do
-    WID=$( wmctrl -lp | sort -k2 | grep -i "Mozilla Firefox" | awk '{print $1, $2}' | grep -P "^\d{1}x(\d|[a-fA-F])+\s1" | awk '{print $1}' )
-done
-wmctrl -i -r $WID -e 0,1367,102,1920,1016
-
-############## Fim Firefox ##############
-
-######################## Fim WorkSpace 1 ######################################
+    ######################## Fim WorkSpace 0 ######################################
 
 
-####################### Inicia WorkSpace 2 ####################################
+    ####################### Inicia WorkSpace 1 ####################################
 
-wmctrl -s 2
+    wmctrl -s 1
 
-############# Inicia procs com Sublime #############
+    ############# Inicia procs com Firefox #############
 
-WID=""
-sublime_text &>/dev/null &
-while [ "$WID" == "" ]; do
-    WID=$( wmctrl -lp | sort -k2 | grep -i "Sublime Text" | awk '{print $1, $2}' | grep -P "^\d{1}x(\d|[a-fA-F])+\s2" | awk '{print $1}' )
-done
-wmctrl -i -r $WID -e 0,1367,102,1920,1016
+    WID=""
+    firefox --new-window "localhost/nube" &>/dev/null &
+    while [ "$WID" == "" ]; do
+        WID=$( wmctrl -lp | sort -k2 | grep -i "Mozilla Firefox" | awk '{print $1, $2}' | grep -P "^\d{1}x(\d|[a-fA-F])+\s1" | awk '{print $1}' )
+    done
+    wmctrl -i -r $WID -e 0,1367,102,1920,1016
 
-############## Fim Sublime ##############
+    ############## Fim Firefox ##############
 
-######################## Fim WorkSpace 2 ######################################
-
-
-####################### Inicia WorkSpace 3 ####################################
-
-wmctrl -s 3
-
-############# Inicia procs com PGAdmin #############
-
-WID=""
-pgadmin3 -s "pgserver" &>/dev/null &
-while [ "$WID" == "" ]; do
-    WID=$( wmctrl -lp | sort -k2 | grep -i "pgAdmin III" | awk '{print $1, $2}' | grep -P "^\d{1}x(\d|[a-fA-F])+\s3" | awk '{print $1}' )
-done
-wmctrl -i -r $WID -e 0,1367,102,960,1016
-
-############## Fim PGAdmin ##############
-
-######################## Fim WorkSpace 3 ######################################
+    ######################## Fim WorkSpace 1 ######################################
 
 
-####################### Inicia WorkSpace 4 ####################################
+    ####################### Inicia WorkSpace 2 ####################################
 
-wmctrl -s 4
+    wmctrl -s 2
 
-############# Inicia procs com Thunderbird #############
+    ############# Inicia procs com Sublime #############
 
-WID=""
-thunderbird &>/dev/null &
-while [ "$WID" == "" ]; do
-    WID=$( wmctrl -lp | sort -k2 | grep -i "Mozilla Thunderbird" | awk '{print $1, $2}' | grep -P "^\d{1}x(\d|[a-fA-F])+\s4" | awk '{print $1}' )
-done
-wmctrl -i -r $WID -e 0,1367,102,1920,1016
+    WID=""
+    sublime_text &>/dev/null &
+    while [ "$WID" == "" ]; do
+        WID=$( wmctrl -lp | sort -k2 | grep -i "Sublime Text" | awk '{print $1, $2}' | grep -P "^\d{1}x(\d|[a-fA-F])+\s2" | awk '{print $1}' )
+    done
+    wmctrl -i -r $WID -e 0,1367,102,1920,1016
 
-############## Fim Thunderbird ##############
+    ############## Fim Sublime ##############
 
-######################## Fim WorkSpace 4 ######################################
-
-
-####################### Inicia WorkSpace 5 ####################################
-
-wmctrl -s 5
-
-############# Inicia procs com Firefox #############
-
-WID=""
-firefox "youtube.com" "trello.com" &>/dev/null &
-while [ "$WID" == "" ]; do
-    WID=$( wmctrl -lp | sort -k2 | grep -i "Mozilla Firefox" | awk '{print $1, $2}' | grep -P "^\d{1}x(\d|[a-fA-F])+\s5" | awk '{print $1}' )
-done
-wmctrl -i -r $WID -e 0,1376,109,960,1016
-
-############## Fim Firefox ##############
-
-######################## Fim WorkSpace 5 ######################################
+    ######################## Fim WorkSpace 2 ######################################
 
 
-####################### Inicia WorkSpace 6 ####################################
+    ####################### Inicia WorkSpace 3 ####################################
 
-wmctrl -s 6
+    wmctrl -s 3
 
-############# Inicia procs com Terminator #############
+    ############# Inicia procs com PGAdmin #############
 
-terminator --geometry=1920x1016+1367+102 -l terminator_webserver
+    WID=""
+    pgadmin3 -s "pgserver" &>/dev/null &
+    while [ "$WID" == "" ]; do
+        WID=$( wmctrl -lp | sort -k2 | grep -i "pgAdmin III" | awk '{print $1, $2}' | grep -P "^\d{1}x(\d|[a-fA-F])+\s3" | awk '{print $1}' )
+    done
+    wmctrl -i -r $WID -e 0,1367,102,960,1016
 
-############## Fim Terminator ##############
+    ############## Fim PGAdmin ##############
 
-######################## Fim WorkSpace 6 ######################################
+    ######################## Fim WorkSpace 3 ######################################
 
 
-####################### Inicia WorkSpace 7 ####################################
+    ####################### Inicia WorkSpace 4 ####################################
 
-wmctrl -s 7
+    wmctrl -s 4
 
-############# Inicia procs com Spotify #############
+    ############# Inicia procs com Thunderbird #############
 
-WID=""
-spotify &>/dev/null &
-while [ "$WID" == "" ]; do
-    WID=$( wmctrl -lp | sort -k2 | grep -i "Spotify" | awk '{print $1, $2}' | grep -P "^\d{1}x(\d|[a-fA-F])+\s7" | awk '{print $1}' )
-done
-wmctrl -i -r $WID -e 0,1367,102,1920,1016
+    WID=""
+    thunderbird &>/dev/null &
+    while [ "$WID" == "" ]; do
+        WID=$( wmctrl -lp | sort -k2 | grep -i "Mozilla Thunderbird" | awk '{print $1, $2}' | grep -P "^\d{1}x(\d|[a-fA-F])+\s4" | awk '{print $1}' )
+    done
+    wmctrl -i -r $WID -e 0,1367,102,1920,1016
 
-############## Fim Spotify ##############
+    ############## Fim Thunderbird ##############
 
-######################## Fim WorkSpace 7 ######################################
+    ######################## Fim WorkSpace 4 ######################################
 
-wmctrl -s 0 # Volta para o primeiro workspace
 
-######################### Fim inicialização de telas ##########################
+    ####################### Inicia WorkSpace 5 ####################################
+
+    wmctrl -s 5
+
+    ############# Inicia procs com Firefox #############
+
+    WID=""
+    firefox "youtube.com" "trello.com" &>/dev/null &
+    while [ "$WID" == "" ]; do
+        WID=$( wmctrl -lp | sort -k2 | grep -i "Mozilla Firefox" | awk '{print $1, $2}' | grep -P "^\d{1}x(\d|[a-fA-F])+\s5" | awk '{print $1}' )
+    done
+    wmctrl -i -r $WID -e 0,1376,109,960,1016
+
+    ############## Fim Firefox ##############
+
+    ######################## Fim WorkSpace 5 ######################################
+
+
+    ####################### Inicia WorkSpace 6 ####################################
+
+    wmctrl -s 6
+
+    ############# Inicia procs com Terminator #############
+
+    terminator --geometry=1920x1016+1367+102 -l terminator_webserver
+
+    ############## Fim Terminator ##############
+
+    ######################## Fim WorkSpace 6 ######################################
+
+
+    ####################### Inicia WorkSpace 7 ####################################
+
+    wmctrl -s 7
+
+    ############# Inicia procs com Spotify #############
+
+    WID=""
+    spotify &>/dev/null &
+    while [ "$WID" == "" ]; do
+        WID=$( wmctrl -lp | sort -k2 | grep -i "Spotify" | awk '{print $1, $2}' | grep -P "^\d{1}x(\d|[a-fA-F])+\s7" | awk '{print $1}' )
+    done
+    wmctrl -i -r $WID -e 0,1367,102,1920,1016
+
+    ############## Fim Spotify ##############
+
+    ######################## Fim WorkSpace 7 ######################################
+
+    wmctrl -s 0 # Volta para o primeiro workspace
+
+    ######################### Fim inicialização de telas ##########################
+else
+    echo -e "É necessário instalar o pacote \"wmctrl\""
+    if [ ${#sbotools} -gt 0 ]; then
+        echo -e "Foi identificado que você possui SBOtools.\nComo root execute \"sboinstall wmctrl\"."
+    fi
